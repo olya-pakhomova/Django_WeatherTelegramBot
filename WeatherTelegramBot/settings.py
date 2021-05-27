7affdf1b3653190520b558d6819674be43729c30
olya-pakhomova <67727594+olya-pakhomova@users.noreply.github.com>
2021-05-27 09:13:59 +0300

    fix

--- WeatherTelegramBot/settings.py
+++ WeatherTelegramBot/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-chz1=-wfp=alv)1ukc9$kpr@07+$^j5vjy)m%i2d9^96q*c2+s'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
 
 # Application definition
