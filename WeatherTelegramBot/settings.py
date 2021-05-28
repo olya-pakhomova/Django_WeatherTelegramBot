3d8156d30e2a7905f1d8e87356a4e3a6d50b0f55
olya-pakhomova <67727594+olya-pakhomova@users.noreply.github.com>
2021-05-28 10:42:59 +0300

    fix

--- WeatherTelegramBot/settings.py
+++ WeatherTelegramBot/settings.py
@@ -87,7 +87,7 @@ WSGI_APPLICATION = 'WeatherTelegramBot.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'weather',
         'HOST': 'localhost',
         'PORT': '5432',
