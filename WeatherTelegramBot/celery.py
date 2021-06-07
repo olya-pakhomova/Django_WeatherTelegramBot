81a21289e5a1d21170cbc43d9299345d181b46d5
olya-pakhomova <67727594+olya-pakhomova@users.noreply.github.com>
2021-06-07 09:02:32 +0300

    fix

--- WeatherTelegramBot/celery.py
+++ WeatherTelegramBot/celery.py
@@ -3,14 +3,17 @@ from celery import Celery
 
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'WeatherTelegramBot.settings')
-app = Celery('WeatherTelegramBot')
-app.config_from_object('django.conf:settings', namespace='CELERY')
-app.autodiscover_tasks()
+celery_app = Celery('WeatherTelegramBot')
+celery_app.config_from_object('django.conf:settings', namespace='CELERY')
+celery_app.autodiscover_tasks()
 
 
-app.conf.beat_schedule = {
+celery_app.conf.beat_schedule = {
     'creating-new-objects': {
         'task': 'tasks.tasks.create_new_object',
         'schedule': 60.0
     }
 }
+
+if __name__ == '__main__':
+    celery_app.start()
