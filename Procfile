ddab6feb430a8310be6efde1da2c4a9351a253e3
olya-pakhomova <67727594+olya-pakhomova@users.noreply.github.com>
2021-06-10 16:56:53 +0300

    fix

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
-web: gunicorn WeatherTelegramBot:app--log-file -
-worker: celery -A WeatherTelegramBot:celery_app worker -l info
+bot: python main.py
+clock: python bot.py
--- bot.py
+++ bot.py
@@ -22,7 +22,7 @@ def get_every_day_weather():
         get_weather(user.user_id, geo_params)
 
 
-schedule.every(2).hours.do(get_every_day_weather)
+schedule.every(3).hours.do(get_every_day_weather)
 
 while True:
     schedule.run_pending()
